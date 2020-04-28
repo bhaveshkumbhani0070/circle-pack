d1f6a28f08e3e5f55e2e30c28a747777bbce9936
bhavesh.kumbhani <bhavesh.kumbhani@artoon.in>
2020-04-28 19:30:11 +0530

    update covid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, ElementRef, Injectable, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/map';
+import { CirclePackComponent } from './circle-pack/circle-pack.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'circle-pack';
+  @ViewChild('iframe') iframe: ElementRef;
+
+  doc: any;
+  compRef: any;
+
+  constructor(
+    private vcRef: ViewContainerRef,
+    private resolver: ComponentFactoryResolver) {}
+ 
+  onLoad() {
+    this.doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentWindow;
+
+    const compFactory = this.resolver.resolveComponentFactory(CirclePackComponent);
+    this.compRef = this.vcRef.createComponent(compFactory);
+    this.doc.body.appendChild(this.compRef.location.nativeElement);
+  }
+  
 }
---
+++ src/app/meta-table/meta-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meta-table',
+  templateUrl: './meta-table.component.html',
+  styleUrls: ['./meta-table.component.css']
+})
+export class MetaTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
